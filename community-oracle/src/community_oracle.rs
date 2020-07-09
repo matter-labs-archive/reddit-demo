c48b43e87752249970744caaa3b9f586186a6c06
Igor Aleksanov <popzxc@yandex.ru>
2020-07-09 13:21:23 +0300

    Add known communities to the Oracle

--- community-oracle/src/community_oracle.rs
+++ community-oracle/src/community_oracle.rs
@@ -4,19 +4,23 @@ use crate::{
     zksync::MintingApi,
 };
 use actix_web::{web, HttpResponse, Responder, Scope};
-use std::sync::Arc;
+use std::{collections::HashSet, sync::Arc};
 
 const DEFAULT_TOKENS_AMOUNT: u64 = 100;
 
 #[derive(Debug, Clone)]
 pub struct CommunityOracle {
+    known_communities: HashSet<String>,
     minter: Arc<MintingApi>,
     tokens_amount: u64,
 }
 
 impl CommunityOracle {
     pub fn new() -> Self {
+        let known_communities = vec!["TestCommunity".to_string()];
+
         CommunityOracle {
+            known_communities: known_communities.into_iter().collect(),
             minter: Arc::new(MintingApi::new()),
             tokens_amount: DEFAULT_TOKENS_AMOUNT,
         }
@@ -26,7 +30,12 @@ impl CommunityOracle {
         oracle: web::Data<Self>,
         request: web::Json<GrantedTokensRequest>,
     ) -> impl Responder {
-        let _request = request.into_inner();
+        let request = request.into_inner();
+
+        if !oracle.known_communities.contains(&request.community_name) {
+            let error = ErrorResponse::error("Invalid community");
+            return HttpResponse::BadRequest().json(error);
+        }
 
         let response = GrantedTokensResponse {
             token_type: "ETH".into(),
@@ -41,6 +50,11 @@ impl CommunityOracle {
         request: web::Json<MintingSignatureRequest>,
     ) -> impl Responder {
         let request = request.into_inner();
+
+        if !oracle.known_communities.contains(&request.community_name) {
+            let error = ErrorResponse::error("Invalid community");
+            return HttpResponse::BadRequest().json(error);
+        }
 
         if !oracle
             .minter

bc4c8056df69a6a22560ebad80986db0e627f032
Igor Aleksanov <popzxc@yandex.ru>
2020-07-13 17:37:20 +0300

    Add reqwest dependency

--- service-provider/src/oracle.rs
+++ service-provider/src/oracle.rs
@@ -1,11 +1,15 @@
+use reqwest::Client;
+
 #[derive(Debug, Clone)]
 pub struct CommunityOracle {
+    client: Client,
     oracle_addr: String,
 }
 
 impl CommunityOracle {
     pub fn new(oracle_addr: impl Into<String>) -> Self {
         Self {
+            client: Client::new(),
             oracle_addr: oracle_addr.into(),
         }
     }
--- service-provider/src/zksync.rs
+++ service-provider/src/zksync.rs
@@ -1,11 +1,13 @@
 use anyhow::Result;
 use chrono::{DateTime, Duration, Utc};
+use reqwest::Client;
 
 // Public re-exports and type declarations to not tie the rest application to the actual zkSync types.
 pub use zksync_models::node::Address;
 
 #[derive(Debug)]
 pub struct ZksyncApp {
+    client: Client,
     rest_api_addr: String,
     json_rpc_addr: String,
 }
@@ -13,6 +15,7 @@ pub struct ZksyncApp {
 impl ZksyncApp {
     pub fn new(rest_api_addr: impl Into<String>, json_rpc_addr: impl Into<String>) -> Self {
         Self {
+            client: Client::new(),
             rest_api_addr: rest_api_addr.into(),
             json_rpc_addr: json_rpc_addr.into(),
         }

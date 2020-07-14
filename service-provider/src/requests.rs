84892ca1b9464875d5f122b85857f1a34408b716
Igor Aleksanov <popzxc@yandex.ru>
2020-07-14 10:17:37 +0300

    Service Provider: Introduce auth data

--- service-provider/src/requests.rs
+++ service-provider/src/requests.rs
@@ -3,14 +3,21 @@ use serde_derive::{Deserialize, Serialize};
 
 pub use community_oracle::requests::{GrantedTokensRequest, MintingSignatureRequest};
 
+/// Type representing the authorization data for the user. In real application, it may be
+/// the authorization token or something similar, currently it's just string that is not checked,
+/// as it's just a demo.
+pub type AuthData = String;
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct DeclareCommunityRequest {
     #[serde(flatten)]
     pub community: Community,
+    pub auth: AuthData,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SubscriptionCheckRequest {
     pub user: Address,
     pub community_name: String,
+    pub auth: AuthData,
 }

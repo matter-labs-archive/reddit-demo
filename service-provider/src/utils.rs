6f1c4394850414c0ab6c3f8fa098fbfd46d165e8
Igor Aleksanov <popzxc@yandex.ru>
2020-07-14 10:01:00 +0300

    Improve response_from_error function

--- service-provider/src/utils.rs
+++ service-provider/src/utils.rs
@@ -2,6 +2,10 @@ use crate::responses::ErrorResponse;
 use actix_web::HttpResponse;
 use anyhow::Error;
 
+/// Converts the `anyhow::Result` into `HttpResponse`.
+/// Since not handled error is uncommon situation, we assume it to be an internal server error.
 pub fn response_from_error(error: Error) -> HttpResponse {
+    log::error!("Request failed with the following error: {}", error);
+    // TODO: Should we really return the error text to the user?
     HttpResponse::InternalServerError().json(ErrorResponse::error(&error.to_string()))
 }
